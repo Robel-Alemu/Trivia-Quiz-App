25e7422312bfad81354df95fe1d8f5c8cfbfbdf2
Robel <robelalemu2372@gmail.com>
2023-10-02 23:20:54 +0300

    customized result board

--- src/components/ResultBoard.tsx
+++ src/components/ResultBoard.tsx
@@ -1,10 +1,49 @@
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Heading,
+  Progress,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 interface Props {
   score: number;
 }
 
 const ResultBoard = ({ score }: Props) => {
-  return <div>{score}</div>;
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      bg="teal"
+      height="100vh"
+    >
+      <VStack
+        width="98vw"
+        height="95%"
+        borderRadius={20}
+        bg="blackAlpha.600"
+        fontSize="5xl"
+        textAlign="center"
+      >
+        {" "}
+        <Heading marginTop={60}> Your Score is {score} %</Heading>
+        <HStack marginTop={20} width="60%" justifyContent="space-evenly">
+          <Button width="13%" height="60px" fontSize="2xl">
+            Done
+          </Button>
+          <Button width="13%" height="60px" fontSize="2xl">
+            Retake
+          </Button>
+        </HStack>
+      </VStack>
+    </Box>
+  );
 };
 
 export default ResultBoard;
